c33020c1014631d45b80fc58fec579588d3164a7
Anirudh Sahu <renudilip19@gmail.com>
2025-07-16 19:54:09 +0530

    quick hack

--- runner.py
+++ runner.py
@@ -14,4 +14,5 @@ def run():
     return jsonify({"output": result})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050)
+    app.run(host="0.0.0.0", port=5050, ssl_context=('certs/cert.pem', 'certs/key.pem'))
+
